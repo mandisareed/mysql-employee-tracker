d5e1a9ff93f8aeb738ea93bec628c6d580979b88
Mandisa Reed <mandisa.reed@gmail.com>
2020-07-16 23:53:30 -0400

    added function to view all depts and return to main menu

--- index.js
+++ index.js
@@ -23,6 +23,49 @@ connection.connect((err) => {
     console.log("Unable to connect to data source. Good bye.");
   } else {
     console.log("Connection established.")
-    //mainMenu();
+    mainMenu();
   }
 });
+
+//list of constants for the dif main menu action options
+const VIEW_DEPARTMENTS = "View all departments";
+const VIEW_ROLES = "View all roles";
+const VIEW_EMPLOYEES = "View all employees";
+const ADD_DEPARTMENT = "Add a department";
+const ADD_ROLE = "Add a role";
+const ADD_EMPLOYEE = "Add an employee";
+const UPDATE_EMPLOYEE_ROLE = "Update an employee's role";
+
+function mainMenu() {
+  return inquirer
+    .prompt({
+      name: "mainAction",
+      type: "list",
+      message: "Choose a task:",
+      choices: [VIEW_DEPARTMENTS, VIEW_ROLES, VIEW_EMPLOYEES, ADD_DEPARTMENT, ADD_ROLE, ADD_EMPLOYEE, UPDATE_EMPLOYEE_ROLE, "EXIT"],
+    })
+    .then((answer => {
+      if (answer.mainAction === VIEW_DEPARTMENTS) {
+        return viewDepartments();
+      }
+      // if (answer.mainAction === VIEW_ROLES) {
+      //   return viewRoles();
+      // }
+      connection.end();
+    }))
+    .catch((error) => {
+      console.log(error);
+      connection.end();
+    });
+};
+
+function viewDepartments() {
+  const allDepartments = `SELECT * FROM department;`;
+  connection.query(allDepartments, (error, deptRows) => {
+    if (error) {
+      throw error;
+    }
+    console.table(deptRows);
+    mainMenu();
+  })
+};
